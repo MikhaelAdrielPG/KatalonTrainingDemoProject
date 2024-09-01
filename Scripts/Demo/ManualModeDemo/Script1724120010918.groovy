5e6893048f4e30e923cffdf3f0ea10d4f4cd32dc
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-09-01 22:28:29 +0700

    failure handling

--- Scripts/Demo/ManualModeDemo/Script1724120010918.groovy
+++ Scripts/Demo/ManualModeDemo/Script1724120010918.groovy
@@ -27,7 +27,7 @@ WebUI.setEncryptedText(findTestObject('Page_OrangeHRM/input_Password_password'),
 
 WebUI.click(findTestObject('Page_OrangeHRM/button_Login'))
 
-WebUI.verifyTextPresent('Dashboard', false)
+WebUI.verifyTextPresent('Dashboard123', false, FailureHandling.OPTIONAL)
 
 WebUI.closeBrowser()
 
